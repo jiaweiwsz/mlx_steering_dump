6bb135bdee2fc479654ae3c9c6e40e5a5c77528f
Jiawei Wang <jiaweiw@nvidia.com>
2021-07-10 17:06:51 +0300

    ingore regc0 display with source vport only
    Signed-off-by: Jiawei Wang <jiaweiw@nvidia.com>

--- src/dr_prettify.py
+++ src/dr_prettify.py
@@ -64,10 +64,9 @@ def pretty_ip_protocol(p):
     else:
         return p
 
-def pretty_source_vport(regc0):
-    source_vport = (int(regc0, 16) >> 16) - 1
-    source_vport = "(0x%x)," % (source_vport-1 if source_vport != 0 else source_vport)
-    return source_vport
+def check_reg0_match(dict):
+    if (int(dict["metadata_reg_c_0"], 16) & 0xffff) == 0:
+        dict.pop("metadata_reg_c_0")
 
 def pretty_l3_type(t):
     switch = {0x1: "800",#"IPv4",
@@ -105,10 +104,6 @@ def prettify_fields(dic):
         if "smac" in j or "dmac" in j:
             dic[j] = pretty_mac(dic[j])
 
-        if "metadata_reg_c_0" in j:
-            dic[j] = pretty_source_vport(dic[j])
-            dic.update({'in_port':dic.pop("metadata_reg_c_0")})
-
         if "l3_type" in j:
             dic[j] = pretty_l3_type(dic[j])
 
@@ -122,7 +117,10 @@ def prettify_fields(dic):
 
 def prettify_tag(tag):
     clean_tag = dict(filter(lambda elem: eval(elem[1]) != 0, tag.items()))
-    prettify_fields(clean_tag)
+    if "metadata_reg_c_0" in clean_tag:
+        check_reg0_match(clean_tag)
+    if len(clean_tag):
+        prettify_fields(clean_tag)
     return clean_tag
 
 
